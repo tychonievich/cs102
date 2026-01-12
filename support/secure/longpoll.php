4eda82e76659763cd1e1f768ae8eabac6286a9fd
Luther Tychonievich <tychonievich@gmail.com>
2026-01-12 09:35:04 -0600

    autocommit

--- support/secure/longpoll.php
+++ support/secure/longpoll.php
@@ -4,12 +4,14 @@ $fh = fopen($fname, "r");
 if (!$fh) die("");
 $when = filemtime($fname);
 fpassthru($fh);
+flush();
 while(true){
   clearstatcache();
   $newwhen = filemtime($fname);
   if ($newwhen != $when) {
     $when = $newwhen;
     fpassthru($fh);
+    flush();
   } else sleep(1); // blocks process, not good at scale
 }
 fclose($fh);
